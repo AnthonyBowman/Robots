4adec9c3a129f887982e3e23f488787aa947e716
Anthony Bowman <brian@sherr.co.uk>
2022-12-17 18:06:39 +0000

    Updates to get Robots ready for all machines

--- ShellScripts/InstallServices.sh
+++
@@ -1,3 +0,0 @@
-sudo cp StreamServer.service /lib/systemd/system/
-sudo cp MQTTServer.service /lib/systemd/system/
-sudo cp UMC.service /lib/systemd/system/
---
+++ Systemd/InstallServices.sh
@@ -0,0 +1,3 @@
+cp StreamServer.service /lib/systemd/system/
+cp MQTTServer.service /lib/systemd/system/
+cp UMC.service /lib/systemd/system/
