b339503758df45285ef225f1b2915b5617086f27
AnthonyBowman <brian@sherr.co.uk>
2023-08-24 12:06:56 +0100

    More work to get consistent build

---
+++ ShellScripts/setupGit.sh
@@ -0,0 +1,6 @@
+# copy this shell script to the home directory - will probably transfer to Desktop, so up one level to home
+sudo apt install git
+git config --global user.name "Anthony"
+git config --global user.email "brian@sherr.co.uk"
+git version
+git clone https://github.com/AnthonyBowman/Robots.git
