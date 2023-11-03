24986ad98230edeb2b68be233972bc9d3c2f2e78
AnthonyBowman <brian@sherr.co.uk>
2023-11-03 14:18:05 +0000

    More progress toward automation

--- Configuration/BWbuild.sh
+++ Configuration/BWbuild.sh
@@ -10,9 +10,14 @@ sudo apt upgrade
 sudo apt install tigervnc-standalone-server
 
 # Set VNC password
-vncpasswd 
+vnc_user="pi"
+vnc_pass="raspberry" 
+
+# Set VNC password 
+echo "$vnc_pass" | vncpasswd -service -user $vnc_user
 
 # Configure systemd service for TigerVNC under Wayland
+sudo rm /etc/systemd/system/vncserver@.service
 sudo tee /etc/systemd/system/vncserver@.service <<END
 [Unit]
 Description=Remote desktop service (VNC)
@@ -34,100 +39,7 @@ END
 # Enable the service
 sudo systemctl enable vncserver@1.service
 
------ Install Git and clone ----
-
-# Prompt for repository choice
-echo "Select repository to clone:"
-select repo in Robots Routers
-do
-  case $repo in
-    Robots)
-      git_repo="https://github.com/AnthonyBowman/Robots.git"
-      target_dir="Robots"
-      break
-      ;;
-    Routers)  
-      git_repo="https://github.com/AnthonyBowman/Routers.git"
-      target_dir="Routers"
-      break
-      ;;
-  esac
-done
-
-# Actuall install git
-sudo apt update
-sudo apt install git
-
-# Add pi user to git group to avoid needing sudo 
-sudo usermod -aG git pi
-git config --global user.name "AnthonyBowman"
-git config --global user.email "brian@sherr.co.uk
-
-
-# Clone selected repository
-git clone $git_repo $target_dir
-
-# After git clone
-
-if [ "$repo" = "Robots" ]; then
-
-  # Push Systemd directory
-  pushd $target_dir/Systemd
-
-  # Run install script
-  bash InstallServices.sh
-  
-  # Run enable script
-  bash EnableServices.sh
-
-  # Return to original directory
-  popd
-
-fi
-
----- Optionally install Mosquitto ----
-
-# Ask if user wants to install Mosquitto
-read -p "Install Mosquitto MQTT? (y/n) " install_mosquitto
-
-# Check response
-if [ "$install_mosquitto" = "y" ]; then
-
-  # Install Mosquitto
-  sudo apt update
-  sudo apt install -y mosquitto mosquitto-clients
-  
-  # Add Python module
-  sudo pip3 install paho-mqtt
-
-  # Configure Mosquitto
-  sudo mosquitto_passwd -c /etc/mosquitto/passwd username
-  sudo chown mosquitto:mosquitto /etc/mosquitto/passwd
-
-  # Update config 
-  echo "listener 1883" | sudo tee -a /etc/mosquitto/mosquitto.conf
-  echo "allow_anonymous true" | sudo tee -a /etc/mosquitto/mosquitto.conf
-  
-  echo "password_file /etc/mosquitto/passwd" | sudo tee -a /etc/mosquitto/mosquitto.conf
-
-  # Enable and restart service
-  sudo systemctl enable mosquitto.service 
-  sudo systemctl restart mosquitto.service
-
-fi
-
----- Install OpenCV ----
-# Install OpenCV dependencies
-sudo apt update 
-sudo apt install -y libopencv-dev python3-opencv
-
-# Also install useful utils
-sudo apt install -y libjpeg-dev libpng-dev libtiff-dev 
-
-# Clean up cache
-sudo apt clean
-
----- Setup Known Wifi ----
+#---- Setup Known Wifi ----
 
 # Prompt to add networks
 read -p "Add known WiFi networks? (y/n) " add_wifi
@@ -151,3 +63,103 @@ if [ "$add_wifi" = "y" ]; then
   nmcli connection modify "$ssid" wifi-sec.key-mgmt wpa-psk wifi-sec.psk "$psk"
   
 fi
+
+# Prompt for type of pi, same as repository choice
+echo "Select Pi type:" 
+read -p "1. Robots 2. Routers 3. Pinage " repo
+
+echo "---- Install Mosquitto ----"
+
+# Check response
+if [ "$repo" = "1"  -o "$repo" = "2" ]; then
+
+  # Install Mosquitto
+  sudo apt update
+  sudo apt install -y mosquitto mosquitto-clients
+  
+  # Add Python module
+  sudo apt install python3-paho-mqtt
+
+  # Configure Mosquitto
+  sudo mosquitto_passwd -c /etc/mosquitto/passwd username
+  sudo chown mosquitto:mosquitto /etc/mosquitto/passwd
+
+  # Update config 
+  echo "listener 1883" | sudo tee -a /etc/mosquitto/mosquitto.conf
+  echo "allow_anonymous true" | sudo tee -a /etc/mosquitto/mosquitto.conf
+  
+  echo "password_file /etc/mosquitto/passwd" | sudo tee -a /etc/mosquitto/mosquitto.conf
+
+  # Enable and restart service
+  sudo systemctl enable mosquitto.service 
+  sudo systemctl restart mosquitto.service
+
+fi
+
+echo ---- Install OpenCV ----
+
+# only need OpenCV for robots
+if [ "$repo" = "1" ]; then
+
+# Install OpenCV dependencies
+sudo apt update 
+sudo apt install -y libopencv-dev python3-opencv
+
+# Also install useful utils
+sudo apt install -y libjpeg-dev libpng-dev libtiff-dev 
+
+# Clean up cache
+sudo apt clean
+
+fi
+
+#----- Install Git and clone ----
+
+case $repo in
+  1|Robots) 
+    git_repo="https://github.com/AnthonyBowman/Robots.git"
+      target_dir="Robots"
+      break
+    ;;
+  2|Routers)
+    git_repo="https://github.com/AnthonyBowman/Routers.git"
+      target_dir="Routers"
+      break
+    ;;
+  *)
+    echo "Invalid selection" 
+    ;;
+esac
+
+# Actually install git
+sudo apt update
+sudo apt install git
+
+# Add pi user to git group to avoid needing sudo 
+sudo usermod -aG git pi
+git config --global user.name "AnthonyBowman"
+git config --global user.email "brian@sherr.co.uk"
+
+
+# Clone selected repository
+git clone $git_repo $target_dir
+
+# After git clone
+
+if [ "$repo" = "1" ]; then
+
+  # Push Systemd directory
+  pushd $target_dir/Systemd
+
+  # Run install script
+  bash InstallServices.sh
+  
+  # Run enable script
+  bash EnableServices.sh
+
+  # Return to original directory
+  popd
+
+fi
+
+echo ---- Finished ----
